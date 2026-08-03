a44bc2f76c766396b371bd2c985489cad63276e4
kmenorio-src <kmenorio@src.pshs.edu.ph>
2026-08-03 09:51:11 +0800

    Create README.py

---
+++ Q1/Activity 0 - Diagnostic Exam/README.py
@@ -0,0 +1,22 @@
+# You are given the scores of 5 students on a given assessment.
+# For each of the scores, output whether they PASSED or FAILED.
+#   - Passing score is 60
+# Calculate the average of the scores, and output them.
+
+import random
+
+# Code template to use with function call to analyze_grades
+def analyze_grades(grades):
+    # Insert your code here
+
+    pass
+
+
+# Grades of students is randomized
+class_grades = [round(random.uniform(0.0, 100.0),2) for _ in range(5)]
+# Example of how to print multiple values in 1 line
+print("Grades:", class_grades)
+
+# Call the function analyze_grades and pass the values
+
+
